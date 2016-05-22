a8037ca2af9ea21f3b595763c09f1719fe0b1883
dotzero <mail@dotzero.ru>
2016-05-22 20:08:32 +0300

    Mocking tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,36 +1,56 @@
-var superagent = require('superagent')
+var request = require('superagent')
+  , mock = require('superagent-mocker')(request)
   , expect = require('expect.js');
 
-describe('NUS rest api', function () {
+describe('Node Url Shortener RESTful API', function () {
   var id;
 
-  it('shorten link', function (done) {
-    superagent.post('http://localhost:3000/api/v1/shorten')
-      .send({
-        long_url: 'https://www.google.com'
-      })
-      .end(function (res) {
-        // console.log(res.body);
-        expect(res.ok).to.be(true);
-        expect(res.body).to.an('object');
-        expect(res.body).not.to.be.empty();
-        expect(res.body).to.have.keys('hash', 'long_url', 'short_url', 'status_code', 'status_txt');
-        id = res.body.hash;
-        done();
-      })
+  beforeEach(function() {
+    mock.clearRoutes();
+    mock.timeout = 0;
   });
 
-  it('expand link', function(done){
-    superagent.post('http://localhost:3000/api/v1/expand')
-      .send({
+  it('should POST /api/v1/shorten', function (done) {
+    mock.post('/api/v1/shorten', function(req) {
+      return {
+        hash: 'MQ==',
+        long_url: req.body.long_url,
+        short_url: 'http://localhost:3000/MQ==',
+        status_code: 200,
+        status_txt: 'OK',
+      };
+    });
+    request.post('/api/v1/shorten', {
+        long_url: 'https://www.google.com'
+      })
+      .end(function(_, data) {
+        // console.log(data);
+        expect(data).to.an('object');
+        expect(data).not.to.be.empty();
+        expect(data).to.have.keys('hash', 'long_url', 'short_url', 'status_code', 'status_txt');
+        id = data.hash;
+        done();
+      });
+  });
+
+  it('should POST /api/v1/expand', function(done){
+    mock.post('/api/v1/expand', function(req) {
+      return {
+        hash: req.body.short_url,
+        long_url: 'https://www.google.com',
+        short_url: 'http://localhost:3000/' + req.body.short_url,
+        status_code: 200,
+        status_txt: 'OK',
+      };
+    });
+    request.post('/api/v1/expand', {
         short_url: id
       })
-      .end(function (res) {
-        // console.log(res.body);
-        expect(res.ok).to.be(true);
-        expect(res.body).to.an('object');
-        expect(res.body).not.to.be.empty();
-        expect(res.body).to.have.keys('hash', 'long_url', 'clicks', 'status_code', 'status_txt');
+      .end(function(_, data) {
+        // console.log(data);
+        expect(data).to.an('object');
+        expect(data).not.to.be.empty();
+        expect(data).to.have.keys('hash', 'long_url', 'short_url', 'status_code', 'status_txt');
         done();
       })
   });
