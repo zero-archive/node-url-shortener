324013dea51ba20059f7b495ce99114fa6dac84a
dotzero <mail@dotzero.ru>
2016-05-23 23:18:23 +0300

    More tests

--- test/redis.test.js
+++ test/redis.test.js
@@ -3,39 +3,56 @@ var request = require('superagent')
   , expect = require('expect.js')
   , RedisModel = require('../lib/redis-model.js');
 
+var mock = function() {};
+
+mock.prototype.incr = function(key, callback) {
+  callback(null, 1);
+};
+
 describe('Test Node Url Shortener - RedisModel', function () {
   var redis, prefix;
 
-  beforeEach(function() {
+  before(function() {
     redis = new RedisModel({ host: 'localhost', port: 6379 });
+    redis.db = new mock();
     prefix = RedisModel._prefix_;
   });
 
-  it('kCounter', function (done) {
+  it('kCounter should return Redis key', function (done) {
     var data = redis.kCounter();
     expect(data).to.be.a('string');
     expect(data).to.be(prefix + 'counter');
     done();
   });
 
-  it('kUrl', function (done) {
+  it('kUrl should return Redis key', function (done) {
     var data = redis.kUrl('http://example.com');
     expect(data).to.be.a('string');
     expect(data).to.be(prefix + 'url:a9b9f04336ce0181a08e774e01113b31');
     done();
   });
 
-  it('kHash', function (done) {
+  it('kHash should return Redis key', function (done) {
     var data = redis.kHash('MQ==');
     expect(data).to.be.a('string');
     expect(data).to.be(prefix + 'hash:MQ==');
     done();
   });
 
-  it('md5', function (done) {
+  it('md5 should MD5 hash', function (done) {
     var data = redis.md5('http://example.com');
     expect(data).to.be.a('string');
     expect(data).to.be('a9b9f04336ce0181a08e774e01113b31');
     done();
+  });
+
+  it('uniqId should unique Redis key', function (done) {
+    redis.uniqId(function(err, hash) {
+      expect(err).to.be(null);
+      expect(hash).to.be.a('string');
+      expect(hash).to.match(/[\w=]+/);
+      done();
+    });
+
   });
 })
