f55b8124c31957f5d7174a49bd4f1cef0fc6b565
dotzero <dotzero@tmtm.ru>
2016-05-23 12:38:50 +0300

    Update template

--- public/js/nus.js
+++ public/js/nus.js
@@ -1,69 +1,58 @@
 (function ($) {
-    var _nus = function (data) {
-        this._api_ = '/api/v1/shorten/';
-        this._urlfield_ = '#urlfield';
-        this._errormsg_ = 'An error occurred shortening that link';
-    };
+  var _nus = function (data) {
+    this._api_ = '/api/v1/shorten/';
+    this._form_ = '#nus';
+    this._errormsg_ = 'An error occurred shortening that link';
+  };
 
-    _nus.prototype.check = function (s) {
-        var regexp = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-        return regexp.test(s);
-    };
+  _nus.prototype.init = function () {
+    this._input_ = $(this._form_).find('input');
 
-    _nus.prototype.alert = function (message, error) {
-        var t = error ? 'alert-error' : 'alert-success';
+    if (!this.check(this._input_.val())) {
+      return this.alert(this._errormsg_, true);
+    }
 
-        $(".alert").alert('close');
+    this.request(this._input_.val());
+  };
 
-        $(this._input_).parent().after('<div class="alert ' + error + '">'
-            + '<button class="close" data-dismiss="alert">×</button>'
-            + message + '</div>');
+  _nus.prototype.check = function (s) {
+    var regexp = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);
+  };
 
-        this.timeout();
-    };
+  _nus.prototype.alert = function (message, error) {
+    var t = error === true ? 'alert-danger' : 'alert-success';
 
-    _nus.prototype.timeout = function () {
-        clearTimeout(this._timeout_);
-        this._timeout_ = setTimeout(function () {
-            $(".alert").alert('close');
-        }, 3000);
-    };
+    $('.alert').alert('close');
+    $('<div class="alert ' + t + ' alert-dismissible" role=alert>'
+      + '<button type=button class=close data-dismiss=alert aria-label=Close><span aria-hidden=true>&times;</span></button>'
+      + message
+      + '</div>').insertBefore(this._form_);
+  };
 
-    _nus.prototype.request = function (url) {
-        var self = this;
-        $.post(self._api_, {long_url: url}, function (data) {
-            console.log(data);
-            if (data.hasOwnProperty('status_code') && data.hasOwnProperty('status_txt')) {
-                if (parseInt(data.status_code) == 200) {
-                    self._input_.val(data.short_url).select();
-                    return self.alert('Copy your shortened url');
-                } else {
-                    self._errormsg_ = data.status_txt;
-                }
-            }
-
-            return self.alert(self._errormsg_, true);
-        }).error(function () {
-            return self.alert(self._errormsg_, true);
-        });
-    };
-
-    _nus.prototype.init = function () {
-        this._input_ = $(this._urlfield_);
-
-        if (!this.check(this._input_.val())) {
-            return this.alert(this._errormsg_, true);
+  _nus.prototype.request = function (url) {
+    var self = this;
+    $.post(self._api_, { long_url: url }, function (data) {
+      if (data.hasOwnProperty('status_code') && data.hasOwnProperty('status_txt')) {
+        if (parseInt(data.status_code) == 200) {
+          self._input_.val(data.short_url).select();
+          return self.alert('Copy your shortened url');
+        } else {
+          self._errormsg_ = data.status_txt;
         }
-
-        this.request(this._input_.val());
-    };
-
-    $(function () {
-        var n = new _nus();
-        $('body').on('click', '#submit', function (e) {
-            e && e.preventDefault();
-            n.init();
-        });
+      }
+      return self.alert(self._errormsg_, true);
+    }).error(function () {
+      return self.alert(self._errormsg_, true);
     });
+  };
+
+  $(function () {
+    var n = new _nus();
+    $(n._form_).on('submit', function (e) {
+      e && e.preventDefault();
+      n.init();
+    });
+  });
 
 })(window.jQuery);
