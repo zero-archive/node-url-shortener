eed24475e174d038e97e353053e4142b868c0f8f
Jack Schlotthauer <jack.schlotthauer@optimizely.com>
2017-07-19 10:35:00 -0700

    updated for mock ui

--- app/components/About.js
+++ app/components/About.js
@@ -1,18 +1,66 @@
 import React from 'react';
 
 const About = () =>
-    <div>
-        Hey! Thanks for using this example. If you like it, consider starring the repo :))
-
-        <div>
-            <iframe
-                src="https://ghbtns.com/github-btn.html?user=jpsierens&repo=webpack-react-redux&type=star&size=large"
-                frameBorder="0"
-                allowTransparency="true"
-                scrolling="0">
-            </iframe>
+  <div>
+    <div className="soft-quad">
+      <header className="background--faint flex soft-quad">
+        <div className="flex flex--1 flex-align--center">
+        <span className="push--right push-half--top">
+          <svg width="120px" viewBox="19.9 225.9 353.2 75.6"><path fill="#0480BA" d="M81.5 260.3l0.7-7.3c-0.1-3.4-0.9-6.4-2.4-9.2l-3.9 5.3c0.5 1.8 0.7 3.7 0.6 5.7 c-0.7 9.2-9.1 16.6-18.6 16.6c-2.2 0-4.3-0.4-6.3-1.2l23-24.1l9.7-9.9l-6.9 3.7c-4.6-5.8-12-9.2-20.5-8.9 c-14.6 0.6-27 12.4-27.6 26.5c-0.7 14 10.5 25 25.1 24.4C67.4 281.4 78.6 272.2 81.5 260.3z M107.7 266.1c0.4-3.9 0.1-6.8-1-8.6 c-1-1.8-2.8-2.7-5.4-2.7c-1.1 0-2.3 0.2-3.4 0.5c-1.1 0.4-2.3 0.9-3.4 1.6l-2 20.3c0.6 0.3 1.4 0.5 2.1 0.6c0.8 0.1 1.8 0.2 2.9 0.2 c3.1 0 5.5-1 7.1-3C106.2 273.2 107.3 270.2 107.7 266.1z M136.1 287.5c-5.1 0-8.8-1.1-10.9-3.2c-2.1-2.1-2.9-5.6-2.5-10.4l1.9-18.9 H120c0.8 3 1 6.7 0.6 10.9c-0.6 6.5-2.8 11.7-6.5 15.6c-3.7 4-7.9 5.9-12.7 5.9c-2 0-3.7-0.2-5.2-0.7c-1.4-0.5-3-1.2-4.5-2.1 L90 301.5H77.4l2.3-22.8c-6.7 9-17.4 14.9-28.9 14.9c-18.5 0-32.2-15.2-30.7-33.9c1.5-18.7 17.7-33.9 36.1-33.9 c9.4 0 17.5 3.9 23.1 10.2l10.6-4.1l-6.3 10.5c0.6 1.2 1.2 2.5 1.6 3.8l10.3 0l-0.4 4.2c2-1.6 4-2.9 6-3.9c2-1 4.3-1.5 6.8-1.5 c2.1 0 3.9 0.4 5.5 1.1l12 0l1.2-11.6h12.6l-1.2 11.6h12.1l-0.9 8.8h-12.1l-1.4 14.3c-0.1 1.4-0.3 2.7-0.3 3.7c-0.1 1.1 0 2 0.3 2.8 c0.3 0.8 0.8 1.5 1.6 2c0.8 0.5 2 0.7 3.7 0.7c0.7 0 1.6-0.1 2.7-0.4c1.1-0.3 1.9-0.6 2.4-0.8h1.1l-0.9 8.9 c-1.4 0.4-2.9 0.6-4.4 0.8C140.2 287.4 138.3 287.5 136.1 287.5z M42 254.8c0.7-9.2 9.1-16.6 18.6-16.6c4.8 0 9 1.9 11.9 4.9 L44.2 264C42.6 261.4 41.7 258.2 42 254.8z M161.5 286.7h-12.6l4-40.4h12.6L161.5 286.7z M166.5 240.5h-13.3l1-9.8h13.3L166.5 240.5 z M212.8 286.7l2-20.2c0.2-2 0.3-3.7 0.4-5c0.1-1.4 0-2.5-0.3-3.3c-0.3-0.9-0.8-1.5-1.5-1.9c-0.7-0.4-1.7-0.6-3.1-0.6 c-1 0-1.9 0.2-2.9 0.6c-1 0.4-2.1 1-3.2 1.8l-2.9 28.7h-12.7l2-20.2c0.2-2 0.3-3.6 0.4-5c0.1-1.4 0-2.5-0.3-3.4 c-0.3-0.9-0.8-1.5-1.5-1.9c-0.7-0.4-1.7-0.6-3.1-0.6c-1 0-2.1 0.2-3.1 0.7c-1 0.5-2 1-3 1.7l-2.9 28.7h-12.6l4-40.4h12.6l-0.4 4.5 c2.2-1.8 4.3-3.1 6.2-4.1c1.9-1 4-1.5 6.3-1.5c2.5 0 4.6 0.6 6.3 1.8c1.8 1.2 3 2.9 3.8 5.3c2.6-2.3 5.1-4 7.4-5.2 c2.3-1.2 4.5-1.8 6.7-1.8c1.9 0 3.5 0.3 5 0.9c1.4 0.6 2.6 1.5 3.5 2.7c1 1.3 1.7 2.8 2.1 4.6c0.4 1.8 0.5 4.1 0.2 7l-2.6 26.3 L212.8 286.7L212.8 286.7z M245.6 240.5h-13.3l1-9.8h13.3L245.6 240.5z M278.3 277c-1.5-3.1-2-6.9-1.6-11.4 c0.7-6.6 3.1-11.9 7.3-15.8c4.2-3.9 9.7-5.9 16.5-5.9c6.2 0 10.7 1.7 13.5 5c2.8 3.3 3.9 8.1 3.2 14.3l-0.5 4.5h-27.6 c-0.1 1.9 0.2 3.4 0.7 4.7c0.5 1.2 1.3 2.2 2.4 3c1 0.7 2.2 1.3 3.6 1.6c1.4 0.3 3 0.5 4.7 0.5c1.5 0 2.9-0.2 4.4-0.5 c1.4-0.3 2.8-0.8 4-1.3c1.1-0.5 2.1-0.9 3-1.5c0.9-0.5 1.7-1 2.3-1.4h1.3l-1 10.4c-1.1 0.4-2.1 0.8-3 1.2c-0.9 0.3-2.1 0.7-3.6 1 c-1.4 0.4-2.8 0.6-4.3 0.8c-1.1 0.2-2.4 0.3-3.8 0.3l-59.2 0h-1.3H228l4-40.4h12.6l-3.1 30.6l20.1-21.1l-15 0.1l1.1-9.3l30.5-0.3 l-0.8 8.2L255.9 277H278.3z M305.1 260.2c0.2-2.7-0.2-4.7-1.3-6.1c-1-1.4-2.7-2.1-5.1-2.1c-2.4 0-4.4 0.7-6 2.2 c-1.5 1.5-2.5 3.5-2.9 6L305.1 260.2L305.1 260.2z M373.1 245.1l-25.9 55.3h-13.5l7.7-15.8l-8.6-31.1l-3.2 32.1h-12.6l5.6-56h12.6 l-1.6 15.6h10.1l6 25.3l10.5-25.3L373.1 245.1L373.1 245.1z"/></svg>
+        </span>
+          <h3 className="flush caps">Go Better</h3>
         </div>
-    </div>;
+        <div className="flex flex--1 flex-align--center">
+          <div className="flex flex--1 lego-input-icon display--inline-block push--right">
+            <input type="text" className="oui-text-input soft-triple--sides" placeholder="Paste URL to create short link" />
+          </div>
 
+          <button className="oui-button oui-button--highlight oui-button--disabled">Add Short Link...</button>
+        </div>
+      </header>
+    </div>
+    <div className="soft-double">
+      <div className="soft-double--sides flex push-double--top">
+        <h2 className="flex flex--1">Short Links</h2>
+        <div className="flex flex--1 flex-align--start flex-justified--end">
+
+          <div className="lego-input-icon display--inline-block">
+            <input type="text" className="oui-text-input soft-triple--sides width--200" placeholder="Search" />
+            <svg className="lego-icon lego-input-icon__left lego-input-icon__muted">
+            </svg>
+          </div>
+
+        </div>
+      </div>
+      <table className="oui-table oui-table--rule oui-table--loose">
+        <thead>
+        <tr>
+          <th className="width--200">Short Link</th>
+          <th>Created By</th>
+          <th>Use Count</th>
+          <th>URL</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>2017adeptkickoff</td>
+          <td>Keith N.</td>
+          <td className="oui-numerical">3</td>
+          <td><a href="">https://docs.google.com/presentation/d/1npOyIson3Uoz2irgWEKRh-JjMCI9jccYiyCd_r6suG4/edit</a></td>
+          <td><button className="oui-button oui-button--tiny float--right">Edit...</button></td>
+        </tr>
+        <tr>
+          <td className="max-width--200 truncate">2017adeptkickoffbutwithanevenlongerslugjustbecause</td>
+          <td className="max-width--200 truncate">Anotherpersonyo B.</td>
+          <td className="oui-numerical">6,665,555</td>
+          <td className="max-width--200 truncate">
+            <a href="">https://docs.google.com/presentation/d/1npOyIson3Uoz2irgWEKRh-JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRhJjMCI9jccYiyCd_r6suG4/edit/presentation/d/1npOyIson3Uoz2irgWEKRh?=JjMCI9jccYiyCd_r6suG4/edit</a></td>
+          <td><button className="oui-button oui-button--tiny float--right">Edit...</button></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>;
 
 export default About;
