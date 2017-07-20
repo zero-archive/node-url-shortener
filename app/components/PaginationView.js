ae63bec896a5cdb080f545c6be1c2447860bdbf9
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-20 13:59:20 -0700

    Fix pagination, need to be connected

--- app/components/PaginationView.js
+++ app/components/PaginationView.js
@@ -43,44 +43,42 @@ class PaginationView extends React.Component {
         this.props.onChangePage(pageOfItems);
     }
 
-    getPager(totalItems, currentPage, pageSize) {
-        // default to first page
-        const current = currentPage || 1;
-        // default page size is 10
-        const pageS = pageSize || 10;
+    getPager(totalItems, currentPage = 1, pageSize = 10) {
         // calculate total pages
         const totalPages = Math.ceil(totalItems / pageSize);
 
-        let startPage = 0;
-        let endPage = 0;
+        let startPage;
+        let endPage;
         if (totalPages <= 10) {
             // less than 10 total pages so show all
             startPage = 1;
             endPage = totalPages;
         } else {
             // more than 10 total pages so calculate start and end pages
-            if (current <= 6) {
+            if (currentPage <= 6) {
                 startPage = 1;
                 endPage = 10;
-            } else if (current + 4 >= totalPages) {
+            } else if (currentPage + 4 >= totalPages) {
                 startPage = totalPages - 9;
                 endPage = totalPages;
             } else {
-                startPage = current - 5;
-                endPage = current + 4;
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
             }
         }
 
         // calculate start and end item indexes
-        const startIndex = (current - 1) * pageS;
-        const endIndex = Math.min(startIndex + pageS - 1, totalItems - 1);
+        const startIndex = (currentPage - 1) * pageSize;
+        const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
         // create an array of pages to ng-repeat in the pager control
         const pages = _.range(startPage, endPage + 1);
+
         // return object with all pager properties required by the view
         return {
             totalItems: totalItems,
-            currentPage: current,
-            pageSize: pageS,
+            currentPage: currentPage,
+            pageSize: pageSize,
             totalPages: totalPages,
             startPage: startPage,
             endPage: endPage,
