dd158b23445cc6bae5ea004a88e6b7da55476541
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-20 14:43:24 -0700

    icons

---
+++ app/components/icons/SearchIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchIcon = () => (
+    <svg className="lego-icon lego-input-icon__left lego-input-icon__muted" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M9.065 18.125C4.066 18.125 0 14.06 0 9.062S4.066 0 9.065 0c4.995 0 9.059 4.065 9.059 9.062s-4.064 9.063-9.059 9.063zM9.065 1C4.618 1 1 4.617 1 9.062s3.618 8.062 8.065 8.062c4.443 0 8.059-3.617 8.059-8.062S13.508 1 9.065 1z"/>
+        <path d="M23 23.5a.502.502 0 0 1-.354-.146l-7.875-7.875a.5.5 0 0 1 .707-.707l7.875 7.875A.5.5 0 0 1 23 23.5z"/>
+    </svg>
+);
+
+export default SearchIcon;
+
